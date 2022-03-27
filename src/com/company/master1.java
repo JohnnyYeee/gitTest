e2d9098e2128f671ae42551a195ac196996c122a
Huasheng Ye <johnnyye9625@gmail.com>
2022-03-27 15:06:11 -0700

    third commit

--- src/com/company/master1.java
+++ src/com/company/master1.java
@@ -4,5 +4,7 @@ public class master1 {
     public static void main(String[] args) {
         System.out.println("first try");
         System.out.println("first try2");
+        System.out.println("firs3");
+        System.out.println("firs  3");
     }
 }
